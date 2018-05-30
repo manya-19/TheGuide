f73c6e7cbb017be50e84923d7270b68ec69a16ca
durzoflint <abhinavu1201@gmail.com>
2018-05-30 14:01:11 +0530

    Retrive Location and fetch address

--- app/src/main/java/flint/durzo/theguide/HomeActivity.java
+++ app/src/main/java/flint/durzo/theguide/HomeActivity.java
@@ -1,13 +1,145 @@
 package flint.durzo.theguide;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity {
+
+    String TAG = "Abhinav";
+    private FusedLocationProviderClient mFusedLocationClient;
+    int LOCATION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        getLocation();
     }
+
+    void getLocation()
+    {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions( this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+        }
+        else {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                                new UserAddress().execute(location);
+                            }
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            getLocation();
+        }
+    }
+
+    class UserAddress extends AsyncTask<Location, Void, Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Location... locations) {
+            Location location = locations[0];
+            String errorMessage = "";
+
+            Geocoder geocoder = new Geocoder(HomeActivity.this, Locale.getDefault());
+
+            List<Address> addresses = null;
+
+            try {
+                addresses = geocoder.getFromLocation(
+                        location.getLatitude(),
+                        location.getLongitude(),
+                        // In this sample, get just a single address.
+                        1);
+            } catch (IOException ioException) {
+                // Catch network or other I/O problems.
+                errorMessage = "Service not available";
+                Log.e(TAG, errorMessage, ioException);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                // Catch invalid latitude or longitude values.
+                errorMessage = "Invalid Cordinates";
+                Log.e(TAG, errorMessage + ". " +
+                        "Latitude = " + location.getLatitude() +
+                        ", Longitude = " +
+                        location.getLongitude(), illegalArgumentException);
+            }
+
+            // Handle case where no address was found.
+            if (addresses == null || addresses.size() == 0) {
+                if (errorMessage.isEmpty()) {
+                    errorMessage = "No Address Found";
+                    Log.e(TAG, errorMessage);
+                }
+                //deliverResultToReceiver(Constants.FAILURE_RESULT, errorMessage);
+            } else {
+                Address address = addresses.get(0);
+                ArrayList<String> addressFragments = new ArrayList<String>();
+
+                // Fetch the address lines using getAddressLine,
+                // join them, and send them to the thread.
+                for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                    addressFragments.add(address.getAddressLine(i));
+                }
+                Log.i(TAG, "Address Found");
+
+                String userAddress = TextUtils.join(System.getProperty("line.separator"),
+                        addressFragments);
+
+                Log.d(TAG, address.getLocality());
+
+                //Log.d(TAG, userAddress);
+
+                /*deliverResultToReceiver(Constants.SUCCESS_RESULT,
+                        TextUtils.join(System.getProperty("line.separator"),
+                                addressFragments));*/
+            }
+                return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+
 }
