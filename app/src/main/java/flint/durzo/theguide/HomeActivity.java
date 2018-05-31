c2dcedff8252458be922ff5b177a5b0f9e46c42a
durzoflint <abhinavu1201@gmail.com>
2018-05-31 12:01:22 +0530

    Adding Location to Server

--- app/src/main/java/flint/durzo/theguide/HomeActivity.java
+++ app/src/main/java/flint/durzo/theguide/HomeActivity.java
@@ -1,30 +1,39 @@
 package flint.durzo.theguide;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Location userLocation = null;
     String TAG = "Abhinav";
     private FusedLocationProviderClient mFusedLocationClient;
     int LOCATION_REQUEST_CODE = 1;
@@ -37,6 +46,16 @@ public class HomeActivity extends AppCompatActivity {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         getLocation();
+
+        Button save = findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText name = findViewById(R.id.name);
+                EditText desc = findViewById(R.id.desc);
+                new RecordLocation().execute(userLocation.getLatitude()+"", userLocation.getLongitude()+"", name.getText().toString(), desc.getText().toString());
+            }
+        });
     }
 
     void getLocation()
@@ -54,10 +73,62 @@ public class HomeActivity extends AppCompatActivity {
                             // Got last known location. In some rare situations this can be null.
                             if (location != null) {
                                 // Logic to handle location object
+                                Toast.makeText(HomeActivity.this, "Location aa gayi hai", Toast.LENGTH_SHORT).show();
+                                userLocation = location;
                                 new UserAddress().execute(location);
                             }
                         }
                     });
+        }
+    }
+
+    class RecordLocation extends AsyncTask<String, Void, Void>{
+        String baseUrl = "http://mobile.tornosindia.com/theguide/";
+        String webPage = "";
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = ProgressDialog.show(HomeActivity.this, "Please Wait", "Adding Location");
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            String lati = strings[0] + "";
+            String longi = strings[1] + "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try
+            {
+                String myURL = baseUrl+"addlocation.php?&name="+strings[2]+"&lati="+lati+"&longi="+longi+"&info="+strings[3];
+                url = new URL(myURL);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                BufferedReader br=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                String data;
+                while ((data=br.readLine()) != null)
+                    webPage=webPage+data;
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progressDialog.dismiss();
+            if (webPage.equals("success"))
+                Toast.makeText(HomeActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(HomeActivity.this, "Some Error Occurred", Toast.LENGTH_SHORT).show();
         }
     }
 
