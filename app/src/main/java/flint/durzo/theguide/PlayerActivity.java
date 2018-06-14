c541c1aee54cec90b285fda6ed3cec42cfaddc01
Manya Gupta <manyaa1910@gmail.com>
2018-06-14 12:39:11 +0530

    UI of player

--- app/src/main/java/flint/durzo/theguide/PlayerActivity.java
+++ app/src/main/java/flint/durzo/theguide/PlayerActivity.java
@@ -1,10 +1,11 @@
 package flint.durzo.theguide;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 
 public class PlayerActivity extends AppCompatActivity {
-
+    TextToSpeech tts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
